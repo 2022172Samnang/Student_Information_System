40d1eee13aaf1ac084ee75f142e0dd8441077df6
2022172Samnang <2022172pheng@aupp.edu.kh>
2022-12-08 18:38:52 +0700

    initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+//#include "Application.cpp"
+#include "app/ApplicationRearrange.cpp"
+using namespace std;
+
+int main(){
+
+    ApplicationRearrange applicationRearrange;
+    applicationRearrange.run();
+
+}
+
+    
---
+++ user_module/GetChoice.cpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <iostream>
+#include <regex>
+#include <limits>
+
+using namespace std;
+
+class GetChoice{
+
+protected:
+
+    int getCommand(int choice){
+        while(true){
+            cout<<"Enter choice: ";
+            cin>>choice;
+            if(cin.fail()){
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                continue;
+            }
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            if(cin.gcount()>1){
+                continue;
+            }
+            if(choice<0){
+                continue;
+            }
+            break;
+        }
+        return choice;
+    }
+};
